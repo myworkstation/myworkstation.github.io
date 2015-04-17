2540e5d534d7d4ae873a9a8ccedb0ee70c4b4793
myworkstation <zhipengx@jumei.com>
2015-04-17 19:06:04 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-var namebtn=document.getElementById("namediv");
-namebtn.onclick=function(){
-	alert("welcome to my webside");
-};
+// var namebtn=document.getElementById("namediv");
+// var picdiv=document.getElementById('picdiv');
+// namebtn.onclick=function(){
+// 	alert("welcome to my webside");
+// };
+var namebtn=$('namediv');
+namebtn.click(function(){
+	alert('welcome!');
+});
