089946fb352b8d979f74afe30c13861b690f935d
myworkstation <zhipengx@jumei.com>
2015-04-17 20:01:38 +0800

    update

--- js/index.js
+++ js/index.js
@@ -11,10 +11,16 @@ namebtn.click(function(){
 	alert('welcome!');
 });
 
-picdiv.click(function(){
-
-});
 
 $(function() {
     $('.banner').unslider();
+});
+
+$('.banner').unslider({
+	speed: 500,               //  The speed to animate each slide (in milliseconds)
+	delay: 1000,              //  The delay between slide animations (in milliseconds)
+	complete: function() {},  //  A function that gets called after every slide animation
+	keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+	dots: true,               //  Display dot navigation
+	fluid: false              //  Support responsive design. May break non-responsive designs
 });
