a13b0dfcca28793f9ba1603277b7906cbd3396e0
myworkstation <zhipengx@jumei.com>
2015-04-24 13:15:46 +0800

    update

--- js/index.js
+++ js/index.js
@@ -37,3 +37,13 @@ $(document).ready(function(){
 		}
 	});
 });
+namebtn.click(function(){
+	$.get('https://api.github.com/gists/1f4d05e72c615872dd5c',function(data,status){
+		alert("Data: " + data + "\nStatus: " + status);
+		if(status==200){
+			var responsejson=eval("("+data+")");
+			alert(responsejson);
+			sideimage.html(responsejson.content);
+		}
+	});
+});
