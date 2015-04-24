e9d8a953639682a6a9ef0856b0bcc05c05cb56de
myworkstation <zhipengx@jumei.com>
2015-04-24 13:42:28 +0800

    update

--- js/index.js
+++ js/index.js
@@ -9,28 +9,29 @@ var picdiv=$('#picdiv');
 
 var sideimage=$('#sideimage');
 
-namebtn.click(function(){
-	alert('welcome!');
+// namebtn.click(function(){
+// 	alert('welcome!');
+// });
+
+$(document).ready(function(){
+	$.get('https://api.github.com/gists/1f4d05e72c615872dd5c',function(data,status){
+		alert("Data: " + data + "\nStatus: " + status);
+		if(status==200){
+			var responsejson=eval("("+data+")");
+			alert(responsejson);
+			sideimage.html(responsejson.content);
+		}
+	});
 });
 
-// $(document).ready(function(){
-// 	$.get('https://api.github.com/gists/1f4d05e72c615872dd5c',function(data,status){
-// 		alert("Data: " + data + "\nStatus: " + status);
-// 		if(status==200){
-// 			var responsejson=eval("("+data+")");
-// 			alert(responsejson);
-// 			sideimage.html(responsejson.content);
-// 		}
-// 	});
-// });
-// namebtn.click(function(){
-// 	alert('thankyou!');
-// 	$.get('https://api.github.com/gists/1f4d05e72c615872dd5c',function(data,status){
-// 		alert("Data: " + data + "\nStatus: " + status);
-// 		if(status==200){
-// 			var responsejson=eval("("+data+")");
-// 			alert(responsejson);
-// 			sideimage.html(responsejson.content);
-// 		}
-// 	});
-// });
+namebtn.click(function(){
+	alert('thankyou!');
+	$.get('https://api.github.com/gists/1f4d05e72c615872dd5c',function(data,status){
+		alert("Data: " + data + "\nStatus: " + status);
+		if(status==200){
+			var responsejson=eval("("+data+")");
+			alert(responsejson);
+			sideimage.html(responsejson.content);
+		}
+	});
+});
